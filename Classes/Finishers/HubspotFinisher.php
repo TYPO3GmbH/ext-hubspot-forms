a2321a43913aa76f3f7c67feb690247615f9d761
Susanne Moog <susanne.moog@typo3.com>
2018-12-12 18:18:31 +0100

    [TASK] Cleanup code
    Squashed commit of the following:
    
    commit 26bc4ed495d6abe4697e0f8f1c277f3ae9d9170e
    Author: Susanne Moog <s.moog@neusta.de>
    Date:   Wed Dec 12 17:45:24 2018 +0100
    
        [TASK] Cleanup all the things

--- Classes/Finishers/HubspotFinisher.php
+++ Classes/Finishers/HubspotFinisher.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package t3g/querybuilder.
+ * This file is part of the package t3g/hubspot_forms.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
--- Classes/ViewHelpers/HubspotPropertiesViewHelper.php
+++ Classes/ViewHelpers/HubspotPropertiesViewHelper.php
@@ -2,7 +2,7 @@
 declare(strict_types = 1);
 
 /*
- * This file is part of the package t3g/querybuilder.
+ * This file is part of the package t3g/hubspot_forms.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
@@ -21,7 +21,7 @@ class HubspotPropertiesViewHelper extends SelectViewHelper
     /**
      * @return array
      */
-    protected function getOptions()
+    protected function getOptions(): array
     {
         $options = parent::getOptions();
         $options['contact'] = 'Contact';
